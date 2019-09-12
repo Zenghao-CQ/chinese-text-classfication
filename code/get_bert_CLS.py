fc17a78c37a5d8d185c1aa5ebfaef0398bd0f78a
Alex <1600012955@pku.edu.cn>
2019-09-13 07:15:30 +0800

    using 'predict'  instead of 'embedding'

--- code/get_bert_CLS.py
+++ code/get_bert_CLS.py
@@ -1,29 +1,36 @@
 # -*- coding: utf-8 -*- 
-from keras_bert import extract_embeddings
+from keras_bert import load_vocabulary, load_trained_model_from_checkpoint, Tokenizer, get_checkpoint_paths
 import numpy as np
 
-file_path = './data/人教+北师大中小学.txt'
-model_path =  "/home/alex/bert_cls/chinese_L-12_H-768_A-12"
-fin = open(file_path,'r',encoding='utf-8')
 
 vec_dim = 768
 sent_len = 220#220
 sent_num = 288#284
 
-#all_ = []
+file_path = '../data/人教+北师大中小学.txt'
+model_path =  "/home/alex/bert_cls/chinese_L-12_H-768_A-12"
+fin = open(file_path,'r',encoding='utf-8')
+paths = get_checkpoint_paths(model_path)
+model = load_trained_model_from_checkpoint(paths.config, paths.checkpoint, seq_len=sent_len)
+
+token_dict = load_vocabulary(paths.vocab)
+tokenizer = Tokenizer(token_dict)
+
+def insidef(text):
+    indices, segments = tokenizer.encode(first=text, max_len=sent_len)
+    predicts = model.predict([np.array([indices]), np.array([segments])])[0]
+    return predicts[0]
+
+all_ = []
 for cnt,line in enumerate(fin):
     print('*****line %d start*****' % cnt)
     line = line[:-1].split('##')# lable & \n
     lable = line[0]
     line = line[1:]
-    vecs = extract_embeddings(model_path, line)
-    vecs = [x[0] for x in vecs]
+    vecs = [insidef(x) for x in line]
     vecs = np.array(vecs)
+    all_.append(vecs)
     print(vecs.shape)
-
-    #res = np.zeros((sent_num-len(vecs),vec_dim))
-    #vecs = np.concatenate((res,vecs),axis=0)
-    #all_.append(vecs)
-
-    np.save("train_line_"+str(cnt)+".npy",vecs)
-#train_X = np.array(all_)
+all_ = np.array(all_)
+print(all_.shape)
+np.save("bert.npy",all_,allow_pickle=True)
